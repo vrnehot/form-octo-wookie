95ec750fcceec5aa2a98c6ef8209afe7c2086685
vrnehot <vrnehot@yandex.ru>
2015-03-30 14:06:39 +0300

    check event values

--- js/fow.js
+++ js/fow.js
@@ -168,7 +168,7 @@ any addEventListener polyfill
 					){ return; }
 					if( keyCode == 86 && isCtrl ){ /* разрешить вставку, но удалить все не цифры */
 						setTimeout(function(){
-							z.inp.value = i.inp.value.replace(/[^0-9\.]+/g,'');
+							z.inp.value = z.inp.value.replace(/[^0-9\.]+/g,'');
 							console.info('Value - ',z.inp.value);
 						},16);
 						return;
